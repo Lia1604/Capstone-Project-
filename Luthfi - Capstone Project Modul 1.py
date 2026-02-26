621ab2ff913ae41a60b55adfea80ffcb996b0b43
Lia1604 <82458503+Lia1604@users.noreply.github.com>
2026-02-26 10:24:15 -0500

    Luthfi - Capstone Project Modul 1.py

--- Luthfi - Capstone Project Modul 1.py
+++ Luthfi - Capstone Project Modul 1.py
@@ -2,7 +2,7 @@
 from prettytable import PrettyTable
 
 
-# DATA PASIEN
+# Data collection menggunakan Nested Dictionary
 data_pasien = {
     "P0001": {"nama": "Budi Santoso", "umur": 35, "jenis_kelamin": "L", "diagnosa": "Tifus"},
     "P0002": {"nama": "Siti Rahma", "umur": 30, "jenis_kelamin": "P", "diagnosa": "Diabetes Tipe 1"},
@@ -13,8 +13,8 @@ data_pasien = {
 }
 
 
-# Function Menampilkan Tabel
-def tampilkan_tabel(data):  
+# Function Menampilkan Tabel menggunakan Pretty table
+def tampilkan_tabel(data): #Function menampilkan data pasien dari dictionary
     table = PrettyTable()
     table.field_names = ["ID Pasien", "Nama", "Umur", "Jenis Kelamin", "Diagnosa"]
     table.align = "c"   
@@ -30,7 +30,7 @@ def tampilkan_tabel(data):
     print("\n", table)
 
 
-# VALIDASI INPUT Y/N
+# VALIDASI INPUT Y/N ketika ada inputan lain selain y atau n, akan looping hingga input benar
 def input_yn(pesan):
     while True:
         pilihan = input(pesan).strip().lower()
@@ -54,23 +54,23 @@ def read_pasien():
         pilihan = input("Pilih menu: ")
 
         if pilihan == "1":
-            if len(data_pasien) == 0:
+            if len(data_pasien) == 0: #kondisi jika tidak ada data pasien
                 print("Data kosong!")
             else:
-                tampilkan_tabel(data_pasien)
+                tampilkan_tabel(data_pasien) #tampilkasn data pasien dari dictionary
 
         elif pilihan == "2":
             print("\nCari berdasarkan: ID, Nama, Umur, Jenis Kelamin atau Diagnosa")
-            kata_cari = input("Masukkan kata kunci pencarian: ").strip()
+            kata_cari = input("Masukkan kata kunci pencarian: ").strip() #kata pencarian akan menghapus spasi di awal dan akhir
 
-            if kata_cari == "":
+            if kata_cari == "": #Kondisi bahwa input tidak boleh kosong
                 print("Kata kunci tidak boleh kosong!")
                 continue
 
-            hasil = {}
-            kata_cari_lower = kata_cari.lower()
-            kata_cari_upper = kata_cari.upper()
-            kata_cari_words = kata_cari_lower.split()
+            hasil = {} #membuat dictionary kosong, pencarian nanti akan disimpan sementara disini
+            kata_cari_lower = kata_cari.lower() #mengubah semua huruf jadi kecil
+            kata_cari_upper = kata_cari.upper() #mengubah semua huruf jadi besar
+            kata_cari_words = kata_cari_lower.split() #mengubah agar pencarian dapat berupa huruf
 
             for id_pasien, detail in data_pasien.items():
                 nama_lower = detail["nama"].lower()
@@ -78,7 +78,7 @@ def read_pasien():
                 nama_match = all(word in nama_lower for word in kata_cari_words)
                 diagnosa_match = all(word in diagnosa_lower for word in kata_cari_words)
 
-                if (
+                if ( #kondisi Or pada pencarian data
                     kata_cari_lower in id_pasien.lower() or
                     nama_match or
                     kata_cari in str(detail["umur"]) or 
@@ -108,34 +108,34 @@ def create_pasien():
             print("Belum ada data pasien.")
 
         id_pasien = input("\nMasukkan ID Pasien Baru: ")
-        if not id_pasien.startswith("P"):
+        if not id_pasien.startswith("P"): #validasi bahwa ID harus diawali dengan huruf P
             print("ID harus diawali huruf P!")
-            if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
+            if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "): #looping jika ingin mengulang proses ketika salaa memasukan data
                 break
             continue
 
         angka = id_pasien[1:]
-        if not angka.isdigit() or len(angka) > 6:
+        if not angka.isdigit() or len(angka) > 6: #validasi setelah harus angka
             print("Setelah P harus diikuti dengan Angka!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
             continue
 
-        if id_pasien in data_pasien:
+        if id_pasien in data_pasien: #validasi bahwa tidak boleh ada data duplikat
             print("ID sudah terdaftar!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
             continue
 
         nama = input("Masukkan Nama: ")
-        if not nama.replace(" ", "").isalpha():   
+        if not nama.replace(" ", "").isalpha(): # validasi nama hanya boleh huruf dan bisa dengan spasi
             print("Nama hanya boleh huruf dan spasi!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
             continue
 
         umur_input = input("Masukkan Umur: ")
-        if not umur_input.isdigit() or int(umur_input) <= 0:
+        if not umur_input.isdigit() or int(umur_input) <= 0: #validasi umur hanya boleh angka dan lebih besar dari 0
             print("Umur harus angka > 0!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
@@ -143,14 +143,14 @@ def create_pasien():
         umur = int(umur_input)
 
         jenis_kelamin = input("Masukkan Jenis Kelamin (L/P): ").upper()
-        if jenis_kelamin not in ["L", "P"]:
+        if jenis_kelamin not in ["L", "P"]: #validasi input hanya boleh "L" atau "P"
             print("Jenis kelamin harus L atau P!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
             continue
 
         diagnosa = input("Masukkan Diagnosa: ")
-        if diagnosa.strip() == "":
+        if diagnosa.strip() == "": #validasi bahwa input tidak boleh kosong
             print("Diagnosa tidak boleh kosong!")
             if not ulang_proses("Ingin mengulang proses tambah pasien? (y/n): "):
                 break
@@ -166,7 +166,7 @@ def create_pasien():
             }
         })
 
-        if input_yn("Simpan Data Pasien Baru? (y/n): ") == "y":
+        if input_yn("Simpan Data Pasien Baru? (y/n): ") == "y": #konfirmasi ulang apakah ingin menyimpan data, jika "y" maka data akan tersimpan
             data_pasien[id_pasien] = {
                 "nama": nama,
                 "umur": umur,
@@ -246,7 +246,7 @@ def delete_pasien():
 
         if id_pasien in data_pasien:
             if input_yn("Yakin ingin menghapus? (y/n): ") == "y":
-                del data_pasien[id_pasien]
+                del data_pasien[id_pasien] # fungis menghapus data pasien
                 print("Data berhasil dihapus!")
         else:
             print("ID tidak ditemukan!")
@@ -259,15 +259,15 @@ def delete_pasien():
 username_benar = "Rsadmin"
 password_benar = "Admin123"
 percobaan = 0
-login_berhasil = False
+login_berhasil = False #status login awal ialah false
 
 while percobaan < 3:
     username = input("Masukkan username: ").strip()
     password = input("Masukkan password: ").strip()
 
-    if username == username_benar and password == password_benar:
+    if username == username_benar and password == password_benar: #kondisi ketika username dan password benar
         print("Login berhasil!")
-        login_berhasil = True
+        login_berhasil = True #status login menjadi true dna masuk ke program utama CRUD
         break
     else:
         percobaan += 1
@@ -278,13 +278,13 @@ while percobaan < 3:
         elif password != password_benar:
             print(f"Login gagal ke-{percobaan}: Password salah!")
 
-if not login_berhasil:
+if not login_berhasil: #kondisi jika gagal login maka akan keluar dari program
     print("Gagal login 3x. Anda telah keluar dari Sistem.")
     exit()
 
 
 # MENU CRUD SETELAH LOGIN
-def main_menu():
+def main_menu(): #Function untuk memanggil ulang function lain dimulai dari Read, Create, Update dan Delete
     while True:
         print("\n===== Data Pasien Rumah Sakit ABC =====")
         print("1. Cari Data Pasien")
@@ -309,4 +309,5 @@ def main_menu():
         else:
             print("Pilihan tidak valid!")
 
-main_menu()
+
+main_menu() #program utama memanggil function main menu yang telah dibuat
